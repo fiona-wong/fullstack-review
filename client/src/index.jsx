ee63ab4425790a10bb2714ee263dab707ce35121
Fiona <feeonawong@gmail.com>
2017-08-03 02:17:09 -0700

    need to add update without refresh

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -14,19 +14,16 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-
     $.ajax({
       url: 'http://localhost:1128/repos',
       method: 'GET',
       success: (data) => {
-        console.log(JSON.parse(data));
         data = JSON.parse(data);
         this.setState({
           repos: data.concat(this.state.repos)
         })
       }
     });
-    
   }
 
   search (term) {
